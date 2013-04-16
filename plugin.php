8b02e5d552dab68cbcaea5f3d3e9dde75f6d4e0d
Beanow <beanow@project-virality.com>
2013-04-16 20:59:51 +0200

    Initial plugin.

---
+++ plugin.php
@@ -0,0 +1,18 @@
+<?php namespace plugins; if(!defined('TX')) die('No direct access.'); ?>
+
+<?php
+/*
+  <!-- Bare distribution -->
+  <script src="<?php echo $plugin ?>lib/jquery.min.js"></script>
+  <script src="<?php echo $plugin ?>jquery.handsontable.js"></script>
+  <script src="<?php echo $plugin ?>lib/bootstrap-typeahead.js"></script>
+  <script src="<?php echo $plugin ?>lib/jQuery-contextMenu/jquery.contextMenu.js"></script>
+  <script src="<?php echo $plugin ?>lib/jQuery-contextMenu/jquery.ui.position.js"></script>
+  <link rel="stylesheet" media="screen" href="<?php echo $plugin ?>jquery.handsontable.css">
+  <link rel="stylesheet" media="screen" href="<?php echo $plugin ?>lib/jQuery-contextMenu/jquery.contextMenu.css">
+*/
+?>
+
+<!-- Full distribution -->
+<script src="<?php echo $plugin ?>dist/jquery.handsontable.full.js"></script>
+<link rel="stylesheet" media="screen" href="<?php echo $plugin ?>dist/jquery.handsontable.full.css">
